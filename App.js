7ef47ba40794dfcc60510ee3a007d712644b4302
eytanellenberg <eytan_ellenberg@yahoo.fr>
2025-12-09 13:04:52 +0200

    Add complete React app structure

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import FairHealthCheck from './FairHealthCheck';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <FairHealthCheck />
+    </div>
+  );
+}
+
+export default App;
