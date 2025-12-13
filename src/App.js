be6852ac35b688b2cf93d3215c748486ae077cc7
eytanellenberg <eytan_ellenberg@yahoo.fr>
2025-12-13 19:31:55 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import React from 'react';
 import FairHealthCheck from './FairHealthCheck';
 import './App.css';
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <FairHealthCheck />
+      <Analytics />   {/* → Analytics doit être placé INSIDE le return */}
     </div>
   );
 }
